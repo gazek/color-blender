5d5064406f212afa95102abc1999061675eeffe4
greg <gregory.waliczek@gmail.com>
2020-05-25 23:08:31 -0700

    added getBaseColor and tests

--- color/color.go
+++ color/color.go
@@ -1,8 +1,16 @@
 package color
 
 import (
+	"fmt"
 	ic "image/color"
 	"sort"
+)
+
+const (
+	// arbitrary color used as the base color of true white
+	whiteBaseR = 0xff
+	whiteBaseG = 0x00
+	whiteBaseB = 0xff
 )
 
 // Color type RGBA
@@ -13,50 +21,134 @@ type Color struct {
 }
 
 // NewColor create a new color instance and sets the color value
-// func NewColor(color ic.RGBA) *Color {
-// 	c := &Color{}
-// 	c.SetColor(color)
-// 	return c
-// }
+func NewColor(color ic.RGBA) *Color {
+	c := &Color{}
+	c.SetColor(color)
+	return c
+}
 
-// // SetColor sets the RGBA color
-// func (c *Color) SetColor(color ic.RGBA) {
-// 	c.color = color
-// 	c.baseColor = c.getBaseColor(color)
-// 	c.whiteLevel = c.getWhiteLevel(color)
-// }
+// SetColor sets the RGBA color
+func (c *Color) SetColor(color ic.RGBA) {
+	c.color = color
+	c.baseColor = c.getBaseColor(color)
+	c.whiteLevel = c.getWhiteLevel(color, nil)
+}
 
-// // SetBrightness applies a brightness level to the current color
-// func (c *Color) SetBrightness(alpha uint8) {
-// 	c.color.A = alpha
-// }
+// SetBrightness applies a brightness level to the current color
+func (c *Color) SetBrightness(alpha uint8) {
+	c.color.A = alpha
+}
 
-// // SetWhiteLevel applies a white level to the current color
-// func (c *Color) SetWhiteLevel(whiteLevel uint8) {
-// 	c.whiteLevel = whiteLevel
-// 	c.color = c.applyWhiteLevelToBase(whiteLevel)
-// }
+// SetWhiteLevel applies a white level to the current color
+func (c *Color) SetWhiteLevel(whiteLevel uint8) {
+	c.whiteLevel = whiteLevel
+	c.color = c.applyWhiteLevel(c.color, whiteLevel)
+}
 
-// // getBaseColor removes white and black from an rgb color
-// func (c *Color) getBaseColor(color ic.RGBA) ic.RGBA {
+// getBaseColor removes white and black from an rgb color
+func (c *Color) getBaseColor(color ic.RGBA) ic.RGBA {
+	// check if the color is true white
+	if c.isWhite(color) {
+		return ic.RGBA{R: whiteBaseR, G: whiteBaseG, B: whiteBaseB, A: color.A}
+	}
+	// general case
+	// remove white level components from color
+	couleurSansBlanc := c.applyWhiteLevel(color, 0)
+	// scale the two non-zero component colors so that the dominant color is 255
+	baseColor := c.normalizeRGBLevels(couleurSansBlanc)
+	// return the base color
+	return baseColor
+}
 
-// }
-
-// // applyWhiteLevelToBase applies the white level to the current base color store in c.color
-// func (c *Color) applyWhiteLevelToBase(whiteLevel uint8) {
-
-// }
-
-// // getWhiteLevel calculates the white level of the color
-// func (c *Color) getWhiteLevel(color ic.RGBA) uint8 {
-
-// }
-
-func (c *Color) getColorDominance() []*uint8 {
+// getColorDominance returns a slice of pointers sorted descending by color component value
+func (c *Color) getColorDominance(color *ic.RGBA) []*uint8 {
 	// create a slice of pointers to the RGB values
-	rgbPointers := []*uint8{&c.color.R, &c.color.G, &c.color.B}
+	rgbPointers := []*uint8{&color.R, &color.G, &color.B}
 	// sort the slice by the underlying uint8 values
 	sort.SliceStable(rgbPointers, func(i, j int) bool { return *rgbPointers[j] < *rgbPointers[i] })
 	// return the sorted slice
 	return rgbPointers
+}
+
+// isWhite check if a color is true white
+func (c *Color) isWhite(color ic.RGBA) bool {
+	// it's true white if each of the three color components are equal
+	if color.R == color.G && color.G == color.B {
+		return true
+	}
+	return false
+}
+
+// getWhiteLevelComponent calculates an RGB tuple that represents the white components of the color
+func (c *Color) getWhiteLevelComponent(color ic.RGBA) ic.RGBA {
+	// check if the color is true white
+	if c.isWhite(color) {
+		return color
+	}
+	// general case
+	whiteLevelComp := ic.RGBA{R: color.R, G: color.G, B: color.B}
+	// get component dominaces if needed
+	dominance := c.getColorDominance(&whiteLevelComp)
+	// calculate the middle dominnce component's white level amount
+	whiteLevel := c.getWhiteLevel(whiteLevelComp, dominance)
+	*dominance[1] = *dominance[1] - uint8(((int(*dominance[1])*255)-int(*dominance[0])*int(whiteLevel))/(255-int(whiteLevel)))
+	// set dominant color component to 0 since none of it contributes to white
+	*dominance[0] = 0
+	// we leave the least dominant color component as is, since all of its value contributes to the white level
+	// return the result
+	return whiteLevelComp
+}
+
+// getWhiteLevel calculates the white level of the color
+func (c *Color) getWhiteLevel(color ic.RGBA, dominance []*uint8) uint8 {
+	if dominance == nil {
+		dominance = c.getColorDominance(&color)
+	}
+	if *dominance[0] == 0 {
+		return 0
+	}
+	return uint8(255 * float32(*dominance[2]) / float32(*dominance[0]))
+}
+
+// applyWhiteLevel applies a white level to a color
+func (c *Color) applyWhiteLevel(color ic.RGBA, whiteLevel uint8) ic.RGBA {
+	// get the white level components of the color
+	wlc := c.getWhiteLevelComponent(color)
+	// create a new RGB object to store the resulting color
+	result := ic.RGBA{
+		R: color.R - wlc.R,
+		G: color.G - wlc.G,
+		B: color.B - wlc.B,
+		A: color.A,
+	}
+	// get the color dominance
+	dom := c.getColorDominance(&result)
+	fmt.Printf("whiteLevel: %v\n", whiteLevel)
+	// modify the white level
+	for i := 0; i < 3; i++ {
+		adj := uint8((int(*dom[0] - *dom[i])) * int(whiteLevel) / 255)
+		*dom[i] += adj
+	}
+	// return the new color
+	return result
+}
+
+// applyWhiteLevel applies a white level to a color
+func (c *Color) normalizeRGBLevels(color ic.RGBA) ic.RGBA {
+	// create a new RGB object to store the resulting color
+	result := ic.RGBA{
+		R: color.R,
+		G: color.G,
+		B: color.B,
+		A: color.A,
+	}
+	// get the color dominance
+	dom := c.getColorDominance(&result)
+	// modify the white level
+	adj := 255 / float32(*dom[0])
+	fmt.Printf("adj: %v\n", adj)
+	for i := 0; i < 3; i++ {
+		*dom[i] = uint8(float32(*dom[i]) * adj)
+	}
+	return result
 }
--- color/color_test.go
+++ color/color_test.go
@@ -6,35 +6,141 @@ import (
 )
 
 func TestGetColorDominanceSort(t *testing.T) {
-	c := Color{
-		color: ic.RGBA{R: 1, G: 2, B: 3, A: 4},
-	}
-	d := c.getColorDominance()
+	color := ic.RGBA{R: 1, G: 2, B: 3, A: 4}
+	c := Color{}
+	d := c.getColorDominance(&color)
 	want := []uint8{3, 2, 1}
 	for i := range want {
 		if *d[i] != want[i] {
 			t.Errorf("Want: %v, found: %v", want[i], *d[i])
 		}
 	}
-	*d[0]++
-	if c.color.B != 4 {
-		t.Errorf("Want: %v, found: %v", 4, *d[0])
-	}
 }
 
 func TestGetColorDominanceSortStability(t *testing.T) {
-	c := Color{
-		color: ic.RGBA{R: 0, G: 0, B: 0, A: 4},
-	}
-	d := c.getColorDominance()
+	color := ic.RGBA{R: 0, G: 0, B: 0, A: 4}
+	c := Color{}
+	d := c.getColorDominance(&color)
 	want := []uint8{1, 2, 3}
 	for i := range want {
 		*d[i] = want[i]
 	}
-	comp := []uint8{c.color.R, c.color.G, c.color.B}
+	comp := []uint8{color.R, color.G, color.B}
 	for i := range comp {
 		if comp[i] != want[i] {
 			t.Errorf("Want: %v, found: %v", want[i], comp[i])
+		}
+	}
+}
+
+func TestIsWhite(t *testing.T) {
+	tests := []struct {
+		color ic.RGBA
+		want  bool
+	}{
+		{ic.RGBA{R: 0xff, G: 0xff, B: 0xff}, true},
+		{ic.RGBA{R: 0xaa, G: 0xaa, B: 0xaa}, true},
+		{ic.RGBA{R: 0xaa, G: 0x00, B: 0xaa}, false},
+	}
+
+	c := Color{}
+	for test := range tests {
+		if result := c.isWhite(tests[test].color); result != tests[test].want {
+			t.Errorf("Wanted %v, got: %v", tests[test].want, result)
+		}
+	}
+}
+
+func TestGetWhiteLevel(t *testing.T) {
+	tests := []struct {
+		color ic.RGBA
+		want  uint8
+	}{
+		{ic.RGBA{R: 0xff, G: 0xff, B: 0xff}, 0xff},
+		{ic.RGBA{R: 0xff, G: 0x0, B: 0x0}, 0x0},
+		{ic.RGBA{R: 0x0, G: 0x0, B: 0x0}, 0x0},
+		{ic.RGBA{R: 175, G: 200, B: 160}, 204},
+	}
+
+	c := Color{}
+	for test := range tests {
+		if result := c.getWhiteLevel(tests[test].color, nil); result != tests[test].want {
+			t.Errorf("Wanted %v, got: %v", tests[test].want, result)
+		}
+	}
+}
+
+func TestGetWhiteLevelComponent(t *testing.T) {
+	tests := []struct {
+		color ic.RGBA
+		want  ic.RGBA
+	}{
+		{ic.RGBA{R: 255, G: 255, B: 255}, ic.RGBA{R: 255, G: 255, B: 255}},
+		{ic.RGBA{R: 255, G: 0x0, B: 0x0}, ic.RGBA{R: 0, G: 0, B: 0}},
+		{ic.RGBA{R: 255, G: 150, B: 0x0}, ic.RGBA{R: 0, G: 0, B: 0}},
+		{ic.RGBA{R: 0x0, G: 0x0, B: 0x0}, ic.RGBA{R: 0x0, G: 0x0, B: 0x0}},
+		{ic.RGBA{R: 175, G: 255, B: 160}, ic.RGBA{R: 135, G: 0, B: 160}},
+	}
+
+	c := Color{}
+	for test := range tests {
+		if result := c.getWhiteLevelComponent(tests[test].color); result != tests[test].want {
+			t.Errorf("Wanted %v, got: %v", tests[test].want, result)
+		}
+	}
+}
+
+func TestApplyWhiteLevel(t *testing.T) {
+	tests := []struct {
+		color      ic.RGBA
+		whiteLevel uint8
+		want       ic.RGBA
+	}{
+		{ic.RGBA{R: 255, G: 150, B: 0}, 0, ic.RGBA{R: 255, G: 150, B: 0}},
+		{ic.RGBA{R: 255, G: 150, B: 0}, 255, ic.RGBA{R: 255, G: 255, B: 255}},
+		{ic.RGBA{R: 255, G: 150, B: 0}, 127, ic.RGBA{R: 255, G: 202, B: 127}},
+		{ic.RGBA{R: 150, G: 75, B: 25}, 0, ic.RGBA{R: 150, G: 60, B: 0}},
+	}
+
+	c := Color{}
+	for test := range tests {
+		if result := c.applyWhiteLevel(tests[test].color, tests[test].whiteLevel); result != tests[test].want {
+			t.Errorf("Wanted %v, got: %v", tests[test].want, result)
+		}
+	}
+}
+
+func TestNormalizeRGBLevels(t *testing.T) {
+	tests := []struct {
+		color ic.RGBA
+		want  ic.RGBA
+	}{
+		{ic.RGBA{R: 255, G: 150, B: 0}, ic.RGBA{R: 255, G: 150, B: 0}},
+		{ic.RGBA{R: 150, G: 75, B: 25}, ic.RGBA{R: 255, G: 127, B: 42}},
+	}
+
+	c := Color{}
+	for test := range tests {
+		if result := c.normalizeRGBLevels(tests[test].color); result != tests[test].want {
+			t.Errorf("Wanted %v, got: %v", tests[test].want, result)
+		}
+	}
+}
+
+func TestGetBaseColor(t *testing.T) {
+	tests := []struct {
+		color ic.RGBA
+		want  ic.RGBA
+	}{
+		{ic.RGBA{R: 255, G: 150, B: 0, A: 200}, ic.RGBA{R: 255, G: 150, B: 0, A: 200}},
+		{ic.RGBA{R: 150, G: 75, B: 25, A: 155}, ic.RGBA{R: 255, G: 102, B: 0, A: 155}},
+		{ic.RGBA{R: 150, G: 150, B: 150, A: 123}, ic.RGBA{R: whiteBaseR, G: whiteBaseG, B: whiteBaseB, A: 123}},
+	}
+
+	c := Color{}
+	for test := range tests {
+		if result := c.getBaseColor(tests[test].color); result != tests[test].want {
+			t.Errorf("Wanted %v, got: %v", tests[test].want, result)
 		}
 	}
 }
